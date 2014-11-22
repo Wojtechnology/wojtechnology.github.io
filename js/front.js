fa24cf73b05fdafb5062e68adc6ca53bc3f6ee99
Wojtechnology <wswiders@uwaterloo.ca>
2014-11-22 12:34:59 -0500

    Dynamic sizing

--- js/front.js
+++ js/front.js
@@ -3,3 +3,17 @@ $('#navBar').affix({
     top: 60,
   }
 })
+
+//Old Function
+/*$(document).ready(function(){
+	$(window).on('resize', function(){
+      	var winWidth = $(this).width() - 15;
+      	if (winWidth <= 1000 && winWidth >= 880){
+			$('.navbar-default').css('margin-top', ((winWidth % 880) / 2) + 'px');
+		}else if(winWidth <= 767){
+			$('.navbar-default').css('margin-top', '0px');
+		}else if(winWidth > 1000){
+			$('.navbar-default').css('margin-top', '60px');
+		}
+	});
+});*/
